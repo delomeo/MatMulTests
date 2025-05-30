d2b3b01c7e44bbc7c16b7afd8a2f3c9b298a7bb8
delomeo <debartolomeo.francesco@gmail.com>
2025-05-30 19:35:11 +0200

    feat: integrate LogMessageObj in fixed size logging

--- log/LoggingUtils.cpp
+++ log/LoggingUtils.cpp
@@ -15,7 +15,20 @@ void LogMessage(std::ofstream& logStream, logLevel level, const std::string& mes
 
 void LogExecutionTimesFixedSize(float Mat1[SIZE][SIZE], float Mat2[SIZE][SIZE], float res[SIZE][SIZE], int iterations) {
     auto operation = [&]() { MatrixMultiplication(Mat1, Mat2, res); };
+
+    // Use LogMessageObj for logging
+    std::ofstream logStream("log/execution_times.log", std::ios::app);
+    if (!logStream.is_open()) {
+        throw std::runtime_error("Failed to open log file: log/execution_times.log");
+    }
+
+    LogMessageObj startLog(logLevel::INFO, "Starting fixed-size matrix multiplication logging.");
+    LogMessage(logStream, startLog);
+
     LogExecutionTimes("log/execution_times.log", operation, iterations, "fixed-size");
+
+    LogMessageObj endLog(logLevel::INFO, "Finished fixed-size matrix multiplication logging.");
+    LogMessage(logStream, endLog);
 }
 
 void LogExecutionTimesDynamic(std::vector<float>& dynamicMat1, std::vector<float>& dynamicMat2, std::vector<float>& resDynamic, int rows, int shared, int cols, int iterations) {
