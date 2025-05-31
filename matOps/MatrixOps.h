e9c34472800fa82904e776944a938eb3d8654d52
delomeo <debartolomeo.francesco@gmail.com>
2025-05-31 18:32:41 +0200

    refactor: generalized mat multiplication via templetes

--- matOps/MatrixOps.h
+++ matOps/MatrixOps.h
@@ -13,4 +13,18 @@ void MatrixMultiplication(float mat1[SIZE][SIZE], float mat2[SIZE][SIZE], float 
 void DynamicMatrixMultiplication(std::vector<float>& res, const std::vector<float>& mat1, const std::vector<float>& mat2, int rows, int shared, int cols);
 
 void PrintDynamicArray(const std::vector<float>& mat, int rows, int cols);
+// Generalized matrix multiplication using templates
+template <typename Matrix>
+Matrix matmul(const Matrix& mat1, const Matrix& mat2);
+
+std::array<std::array<float, SIZE>, SIZE> matmul(
+    const float mat1[SIZE][SIZE],
+    const float mat2[SIZE][SIZE]);
+    
+// Overloading for std::vector<float>
+std::vector<float> matmul(const std::vector<float>& mat1, const std::vector<float>& mat2, int rows, int shared, int cols);
+
+// Overloading for Eigen matrices
+Eigen::MatrixXd matmul(const Eigen::MatrixXd& mat1, const Eigen::MatrixXd& mat2);
+
 #endif // MATRIX_OPS_H
